86400d64a8ed86a381456bfb07f1faa058d098c5
kaushik-manoharan <kaushik.manoharan@tracxn.com>
2020-09-22 16:23:26 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Car from './components/classComp';
+import Home from './components/components';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [st,setSt] = useState({stationStatus: true,serviceStation: 'Meenambakkam, Chennai,TN'})
 
   
   return (
-    <div className="App">
-      <header className="App-header">
-        <Car properties = {st}/>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+        // {/* <Car properties = {st}/> */}
+        <Home/>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ src/components/components/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Navigation from './utils/navbar'
+export default function Home()
+{
+    return (
+        <Navigation/>
+    )
+}
